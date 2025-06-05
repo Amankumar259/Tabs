269ed62ff085b44621306e2527f2dfea6cf97261
Aman Kumar <157944056+Amankumar259@users.noreply.github.com>
2025-06-05 15:27:05 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Profile from "./Components/Profile";
+import Sidebar from "./Components/Sidebar";
+
+const App = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Profile />
+    </div>
+  );
+};
+export default App;
---
+++ src/Components/About.tsx
@@ -0,0 +1,31 @@
+const About = () => {
+  return (
+    <section className="bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4xl max-auto">
+        <h2 className="text-3xl font-extrabold text-gray-900 mb-6">
+          About Channel
+        </h2>
+        <p className="text-lg text-gray-700">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi culpa
+          enim aperiam delectus recusandae molestiae exercitationem, amet iste
+          autem debitis sapiente? Doloribus eaque impedit illo fuga sint cum,
+          maiores debitis. Deserunt error, voluptatibus fugit dolor aut alias
+          eos porro vel eaque cupiditate ipsum possimus nostrum vitae cum quod
+          omnis adipisci sit, in, nihil quisquam repellendus rem blanditiis
+          velit. Velit, iure. Temporibus, voluptates consequatur vel reiciendis,
+          cupiditate facilis distinctio quos pariatur amet possimus corrupti
+          fugit molestias sit illum laborum modi debitis quas nostrum,
+          consectetur repudiandae ab itaque? Esse ex odit voluptates?
+        </p>
+
+        <p className="mt-5 text-lg text-gray-700">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          necessitatibus asperiores soluta, animi rerum commodi consequatur
+          doloribus minima a, dolor libero, deleniti dolores officia ducimus
+          sunt dolorum quis modi ipsa.
+        </p>
+      </div>
+    </section>
+  );
+};
+export default About;
